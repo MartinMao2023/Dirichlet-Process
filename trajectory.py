61d73aa815e75a51fa49214ec598dbdfca8ef923
Runjun <rm522@ic.ac.uk>
2023-07-24 16:58:47 +0100

    optimised for robot trajectory

--- trajectory.py
+++ trajectory.py
@@ -1,149 +1,164 @@
-import torch
-import torch.nn as nn
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import random
 import math
 from datetime import datetime
+import numpy.linalg as lg
+from scipy.optimize import minimize
 
 
 
-x = np.linspace(0, 3.1416, 200, dtype=np.float32)
-sinx = np.sin(x)
-# approx_sinx = x/2 - np.power(x/2, 3) / 6 
-# # + np.power(x, 9) / 362880
-approx_sinx = x - 0.19895929 * np.power(x, 2.43713223)
-# approx_sinx = x - 0.1584253 * np.power(x, 2.85328487)
 
-# plt.plot(x, sinx)
-# plt.plot(x, approx_sinx)
-# plt.plot(x*57.3, (approx_sinx + 1e-10) / (sinx + 1e-10))
-# plt.show()
+def loss(RAB, x, y, weights):
+    R, A, B = RAB
+    x_A_2 = np.square(x - A)
+    y_B_2 = np.square(y - B)
+    R_2 = R**2
+    loss = R_2 + x_A_2 + y_B_2 - 2*np.sqrt(R_2*(x_A_2 + y_B_2))
+    return np.mean(loss*weights)
 
-# plt.plot(x, np.sin(x))
-# plt.plot(x, x - x**3 / 6 + x**5/120 - x**7/5040)
-# # plt.plot(x, np.sin(x/2)**3)
-# # plt.plot(x, )
-# plt.show()
+
+
+def jac(RAB, x, y, weights):
+    R, A, B = RAB
+    x_A_2 = np.square(x - A)
+    y_B_2 = np.square(y - B)
+    denominator = np.sqrt(x_A_2 + y_B_2) + 1e-6
+    L_R = np.mean((2*R - 2*denominator)*weights)
+    L_A = np.mean(2*(A - x)*(1 - R / denominator)*weights)
+    L_B = np.mean(2*(B - y)*(1 - R / denominator)*weights)
+    return np.array([L_R, L_A, L_B])
+
 
 
 def calculate_arc(x, y, t):
-    """ This function is very hard to understand.
-    For now just accept it.
-    """
+    data_size = len(t)
+    row_square = (x**2 + y**2).reshape(-1, 1)
+    X_matrix = np.zeros((data_size, 2), dtype=np.float32)
+    X_matrix[:, 0] = x
+    X_matrix[:, 1] = y
+    noise = np.array([[1e-4, 0], [0, 1e-4]], dtype=np.float32)
+    W = lg.inv(X_matrix.T @ X_matrix + noise) @ X_matrix.T @ row_square
 
-    t1 = t.copy()
-    t2 = np.power(t1, 2.43713223)
-    row = np.sqrt(x**2 + y**2)
+    ############################
+    ##    initial estimate    ##
+    ############################
+    A, B = W[:, 0]/2
 
-    t1_square = np.sum(t1**2)
-    t2_square = np.sum(t2**2)
-    t1_t2 = np.sum(t1 * t2)
-    row_t1 = np.sum(row * t1)
-    row_t2 = np.sum(row * t2)
-    ratio = (row_t2*t1_square - row_t1*t1_t2) / (row_t1*t2_square - row_t2*t1_t2)
+    #########################
+    ##    minimize loss    ##
+    #########################
 
-    rough_w = 0 if ratio > 0 else math.pow(-13.61*ratio, 0.6958302) + 1e-10
+    weights = 1/(0.4*t / np.max(t) + 0.6)
 
-    weight = np.where(t*rough_w > np.pi, 0, 1)
-    t2 = np.power(t1, 2.8532847)
 
-    t1_square = np.sum(weight * t1**2)
-    t2_square = np.sum(weight * t2**2)
-    t1_t2 = np.sum(weight * t1 * t2)
-    row_t1 = np.sum(weight * row * t1)
-    row_t2 = np.sum(weight * row * t2)
+    res = minimize(fun=loss, 
+                   x0=np.array([math.sqrt(A**2 + B**2), A, B]), 
+                   args=(x, y, weights), 
+                   method="L-BFGS-B", 
+                   bounds=((1e-4, 1e3), (-1e3, 1e3), (-1e3, 1e3)),
+                   jac=jac)
+    
+    R, A, B = res.x
 
-    det = t1_square*t2_square - t1_t2**2
-    v = (row_t1*t2_square - row_t2*t1_t2) / det
-    ratio = (row_t2*t1_square - row_t1*t1_t2) / (det * v)
+    ###########################
+    ##    calculate omega    ##
+    ###########################
 
-    w = 1e-5 if ratio > 0 else math.pow(-22.8071*ratio, 0.53958) 
+    xy = np.vstack((x-A, y-B), dtype=np.float32).T
+    modulus = np.sqrt(np.sum(xy**2, axis=1))
+    xy_ = xy.copy()
+    modulus_ = modulus.copy()
+    xy_[1:] = xy[:-1]
+    modulus_[1:] = modulus[:-1]
 
-    cos_beta = x / (row + 1e-10)
-    sin_beta = y / (row + 1e-10)
-    cost = np.cos(0.5*t1*w)
-    sint = np.sin(0.5*t1*w)
+    sin_angles = np.clip(np.cross(xy_, xy) / (modulus*modulus_), -1, 1)
+    delta_angles = np.arcsin(sin_angles)
 
-    sin_weight = row**2 / (cos_beta**2 * sin_beta**2 + 1 + sin_beta**4 - 2*sin_beta**2 + 0.01)
-    cos_weight = row**2 / (cos_beta**2 * sin_beta**2 + 1 + cos_beta**4 - 2*cos_beta**2 + 0.01)
+    t_ = t.copy()
+    t_[1:] = t[:-1]
+    delta_t = t - t_
 
-    cos_det = np.sum(cos_weight*sint**2)*np.sum(cos_weight*cost**2) - np.sum(cos_weight*sint*cost)**2
-    sin_det = np.sum(sin_weight*sint**2)*np.sum(sin_weight*cost**2) - np.sum(sin_weight*sint*cost)**2
+    angle_weights = 1.01 - sin_angles**2
 
-    sinphi = (np.sum(sin_weight*sin_beta*cost)*np.sum(sin_weight*sint**2) - np.sum(sin_weight*sin_beta*sint)*np.sum(sin_weight*sint*cost)) / sin_det
-    sec_sinphi = (np.sum(cos_weight*cos_beta*sint)*np.sum(cos_weight*cost**2) - np.sum(cos_weight*cos_beta*cost)*np.sum(cos_weight*sint*cost)) / cos_det
 
-    sec_cosphi = (np.sum(sin_weight*sin_beta*sint)*np.sum(sin_weight*cost**2) - np.sum(sin_weight*sin_beta*cost)*np.sum(sin_weight*sint*cost)) / sin_det
-    cosphi = (np.sum(cos_weight*cos_beta*cost)*np.sum(cos_weight*sint**2) - np.sum(cos_weight*cos_beta*sint)*np.sum(cos_weight*sint*cost)) / cos_det
+    w =  np.sum(delta_t * delta_angles * angle_weights) / np.sum(delta_t**2 * angle_weights) 
 
-    phi = math.atan2(sinphi, cosphi)
 
-    if abs(cosphi*sec_cosphi) >= abs(sec_sinphi*sinphi):
-        w = w if cosphi*sec_cosphi > 0 else -w
-    else:
-        w = w if sinphi*sec_sinphi < 0 else -w
-        
+    ###############################
+    ##    calculate v and phi    ##
+    ###############################
+
+    v = np.abs(R*w)
+
+    phi = math.atan2(-A, B) if w > 0 else math.atan2(A, -B)
+
+    # print("w:", round(w*57.3, 4))
+    # print("v:", round(v, 4))
+    # print("phi", round(phi*57.3, 4))
     return v, w, phi
 
 
 
+if __name__ == "__main__":
+    # x = np.linspace(0, 3.1416, 128)
+    # x = 4 - np.cos(x)*4
+    # # y = np.sqrt(16 - x**2) - 4
+    # # y = 4 - np.sqrt(16 - x**2)
+    # y = np.sqrt(16 - (x - 4)**2) 
+    # # y = 10*x
 
 
-R = 100
-angle = math.pi * 0.5
-arc = R * angle
-print("arc:", round(arc, 2))
-print("real v:", round(arc / 10, 4))
-print("real w:", round(-angle / 10, 4))
-total_time = 10 
-phi = math.pi * 2/ 3
 
-ox = R * math.sin(phi)
-oy = -R * math.cos(phi)
+    # # # plt.plot(x, y)
+    # # # plt.show()
+
+    # v, w, phi = calculate_arc(x, y, np.linspace(0, 1, 128))
+    # print("w:", round(w*57.296, 4))
+    # print("v:", round(v, 4))
+    # print("phi", round(phi*57.296, 4))
 
 
-sample_amount = 64
-t = np.arange(sample_amount) / sample_amount * total_time
-# print(np.max(t))
+    angle = math.pi * 1.5    # <--- total angle. In this case it is one and half a circle
+    arc = 300    # <--- arc length
+    R = arc / angle 
+    # print("real R:", round(R, 4))
 
-alpha = -np.arange(sample_amount) * (angle / sample_amount) + phi + math.pi / 2
-fluctuation_cycle = 10
-fluctuation_amplitude = 0.2
-sideways_amplitude = 0.1
-add_fluctuation = True
+    total_time = 10    # <--- time for this trajectory
+    phi = math.pi * 5 / 12    # <--- initial angle, 75 degree
 
-if add_fluctuation:
+
+    sample_amount = 64    # <--- number of samples taken from this trajectory
+
+
+    t = np.arange(sample_amount) / sample_amount * total_time
+    alpha = -np.arange(sample_amount) * (angle / sample_amount) + phi + math.pi / 2
+
+    ox = R * math.sin(phi)
+    oy = -R * math.cos(phi)
+
+
+    fluctuation_cycle = 5    # <--- fluctuate 5 cycles during the trajectory
+    fluctuation_amplitude = 0.2    # <--- forward fluctation amplitude is 20% of total angle 
+    sideways_amplitude = 0.1    # <--- sideways fluctuation is 10% of arc length
+
+
+    ## | <---  we add the fluctuations  ---> | ##
     alpha += angle*fluctuation_amplitude*np.sin(2*math.pi*t/total_time*fluctuation_cycle)
-    plt.plot(t, alpha)
-    plt.show()
-
-    # px = np.cos(alpha)*R + ox 
-    # py = np.sin(alpha)*R + oy
-
-
     px = np.cos(alpha)*(R + arc*sideways_amplitude*np.sin(2*math.pi*t/total_time*fluctuation_cycle)) + ox 
     py = np.sin(alpha)*(R + arc*sideways_amplitude*np.sin(2*math.pi*t/total_time*fluctuation_cycle)) + oy
-else:
-    px = np.cos(alpha)*R + ox 
-    py = np.sin(alpha)*R + oy
 
 
-px += np.random.normal(0, 1, sample_amount)
-py += np.random.normal(0, 1, sample_amount)
+    # and also the noise
+    px += np.random.normal(0, 5, sample_amount)
+    py += np.random.normal(0, 5, sample_amount)
 
-if add_fluctuation:
-    plt.plot(t, np.sqrt(px**2 + py**2))
-    plt.show()
+    v, w, phi = calculate_arc(px, py, t)
+    # print("w:", round(w*57.296, 4))
+    print("w:", round(w, 4))
+    print("v:", round(v, 4))
+    print("phi", round(phi*57.296, 4))
 
-v, w, estimated_phi = calculate_arc(px, py, t)
-
-
-print("v:", round(v, 4))
-print("w:", round(w, 4))
-print("phi:", round(estimated_phi*57.3, 4))
-# print(round(v, 4), round(w, 4))
-# print("R:", v / w)
 
 
 
